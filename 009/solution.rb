1479c5ce52fd6fd8b4a565dc0e55298e1ebed6e2
adamgibbons <adam.d.gibbons@gmail.com>
2013-07-04 00:01:40 -0400

    solves problem 9

--- 009/solution.rb
+++ 009/solution.rb
@@ -8,3 +8,32 @@
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
 
+
+
+
+def isNatural(a, b)
+  return true if (Math.sqrt(a*a + b*b) % 1 == 0)
+  false
+end
+
+def isCouplet(a, b, c)
+  return true if ((1000 - c) == (a + b))
+end
+
+ab_list = []
+
+for a in 1..500
+  for b in a..500
+    if isNatural(a, b)
+      ab_list.push([a, b])
+    end
+  end
+end
+
+ab_list.each do |n|
+  a, b = n.first, n.last
+  c = Math.sqrt(a*a + b*b)
+  if isCouplet(a, b, c)
+    puts a*b*c.to_i
+  end
+end
