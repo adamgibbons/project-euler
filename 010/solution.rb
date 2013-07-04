6e4d9782ca379be54ae932b5c9d8a53b8771b8d8
Adam Gibbons <adam.d.gibbons@gmail.com>
2013-07-04 12:46:43 -0400

    adds array accumulator to output real answer

--- 010/solution.rb
+++ 010/solution.rb
@@ -24,4 +24,14 @@ def sieveUpTo(max)
   primes_list = primes_list - temp_list
 end
 
-puts sieveUpTo(100000)
+def getSumOf(arr)
+  accumulator = 0
+  arr.each {|n| accumulator += n}
+  accumulator
+end
+
+sieved_primes = sieveUpTo(2000000)
+puts getSumOf(sieved_primes)
+
+# ruby solution.rb  14.88s user 0.16s system 99% cpu 15.055 total
+# that can definitely get better but, ya know, not bad
