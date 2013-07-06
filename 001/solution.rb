f996a1604b1d48e46c6a6108ceb4dfacdf395cf0
Adam Gibbons <adam.d.gibbons@gmail.com>
2013-07-06 13:34:23 -0400

    adds efficient solution

--- 001/solution.rb
+++ 001/solution.rb
@@ -1,6 +1,17 @@
+# brute force
+
 sum = 0
 (1...1000).each do |n|
   sum += n if (n % 5 == 0) or (n % 3 == 0)
 end
-
 puts sum
+
+# more efficient
+$TARGET = 999
+
+def sumDivisibleBy(n)
+  p = $TARGET / n
+  return n * (p * (p + 1)) / 2
+end
+
+puts sumDivisibleBy(3) + sumDivisibleBy(5) - sumDivisibleBy(15)
