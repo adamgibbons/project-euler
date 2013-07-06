3fd5e3b993089dbc0daeb8b0f1354a56ac822d67
Adam Gibbons <adam.d.gibbons@gmail.com>
2013-07-05 20:02:08 -0400

    adds diagonal checks

--- 011/solution.rb
+++ 011/solution.rb
@@ -75,59 +75,44 @@ def greatestVerticalProduct(grid)
   product_list.max
 end
 
+# DIAGONAL
 
+def checkDiagonalTLtoBR(grid)
+  product_list = []
+  height, length = 20, 20
+  y, x = 0, 0
+  while y < (height - 3) do
+    while x < (length - 3) do
+      letter = [grid[y][x], grid[y + 1][x + 1], grid[y + 2][x + 2], grid[y + 3][x + 3]]
+      product_list.push(getProduct(letter))
+      x += 1
+    end
+    y += 1
+  end
+  product_list.max
+end
 
+def reverseGrid(grid)
+  reversed_grid = []
+  grid.each do |line|
+    reversed_grid.push(line.reverse)
+  end
+  reversed_grid
+end
 
-
-
-
-
-
-# def checkGridTtoB(grid)
-#   height = 20
-#   i = 0
-#   product_list = []
-#   while i < height do
-#     product_list += checkLineTtoB(grid)
-#     i += 1
-#   end
-#   product_list
-# end
-
-
-
-# def checkDiagonalTLtoBR(grid)
-#   product_list = []
-#   height, length = 20, 20
-#   y, x = 0, 0
-#   while y < (height - 3) do
-#     while x < (length - 3) do
-#       letter = [grid[y][x], grid[y + 1][x + 1], grid[y + 2][x + 2], grid[y + 3][x + 3]]
-#       product_list.push(getProduct(letter))
-#       x += 1
-#     end
-#     y += 1
-#   end
-#   product_list
-# end
-
-# def reverseGrid(grid)
-#   reversed_grid = []
-#   grid.each do |line|
-#     reversed_grid.push(line.reverse)
-#   end
-#   reversed_grid
-# end
-
-# def checkDiagonalTRtoBL(grid)
-#   reversed_grid = reverseGrid(grid)
-#   checkDiagonalTLtoBR(reversed_grid)
-# end
-
-
+def checkDiagonalTRtoBL(grid)
+  reversed_grid = reverseGrid(grid)
+  checkDiagonalTLtoBR(reversed_grid)
+end
 
 grid = readGrid('number_grid.txt')
-puts greatestHorizontalProduct(grid)
-puts greatestVerticalProduct(grid)
 
+greatest_horizontal_product = greatestHorizontalProduct(grid)
+greatest_vertical_product = greatestVerticalProduct(grid)
+greatest_diagonal_l_to_r_product = checkDiagonalTLtoBR(grid)
+greatest_diagonal_r_to_l_product = checkDiagonalTRtoBL(grid)
 
+puts greatest_horizontal_product
+puts greatest_vertical_product
+puts greatest_diagonal_r_to_l_product
+puts greatest_diagonal_l_to_r_product
