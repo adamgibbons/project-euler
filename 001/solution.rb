3da4e5ae95d4164de8ffeba9488d9ea8cb1b43d9
Adam Gibbons <adam.d.gibbons@gmail.com>
2013-07-06 12:35:04 -0400

    simplifies code

--- 001/solution.rb
+++ 001/solution.rb
@@ -1,13 +1,6 @@
-list = []
+sum = 0
 (1...1000).each do |n|
-  if (n % 5 == 0) or (n % 3 == 0)
-		list.push n
-	end
+  sum += n if (n % 5 == 0) or (n % 3 == 0)
 end
 
-ans = 0
-list.each do |item|
-	ans += item
-end
-
-puts ans
+puts sum
